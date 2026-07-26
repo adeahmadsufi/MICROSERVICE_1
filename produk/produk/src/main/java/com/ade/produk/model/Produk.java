343658b4c15df8ecffc56556105a8af9fb0ca01b
Ade Ahmad Sufi <adeahmadsufi2@gmail.com>
2026-07-27 06:26:57 +0700

    Add files via upload

--- produk/produk/src/main/java/com/ade/produk/model/Produk.java
+++ produk/produk/src/main/java/com/ade/produk/model/Produk.java
@@ -1,17 +1,5 @@
 package com.ade.produk.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Data;
-import jakarta.persistence.GenerationType;
-@Data
-@Entity(name = "produks")
 public class Produk {
- @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nama;
-    private String satuan;
-    private Double harga;
+
 }
--- produk/produk/src/main/java/com/ade/produk/repository/ProdukRepository.java
+++ produk/produk/src/main/java/com/ade/produk/repository/ProdukRepository.java
@@ -1,11 +1,5 @@
 package com.ade.produk.repository;
 
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ade.produk.model.Produk;
-@Repository
-public interface ProdukRepository extends JpaRepository<Produk, Long> {
+public class ProdukRepository {
 
 }
